1f39fbdc3b98af5551a73fad044e64ecb7031a99
Ingo Reitz <9l@9lo.re>
2025-08-28 20:58:04 +0200

    cosmic-idle: 999 -> epoch-1.0.0-alpha.7-unstable-2025-02-25
    Diff: https://github.com/pop-os/cosmic-idle/compare/267bb837f127eb805a17250ebcad02db57eb72cb...267bb837f127eb805a17250ebcad02db57eb72cb

--- pkgs/cosmic-idle/default.nix
+++ pkgs/cosmic-idle/default.nix
@@ -14,7 +14,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-idle";
-  version = "999";
+  version = "epoch-1.0.0-alpha.7-unstable-2025-02-25";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
