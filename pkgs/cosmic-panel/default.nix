20c83b17fc5eb4ed8c1ee12f5987dbaeed508868
Ingo Reitz <9l@9lo.re>
2025-08-28 21:01:40 +0200

    cosmic-panel: 999 -> epoch-1.0.0-alpha.7-unstable-2025-08-27
    Diff: https://github.com/pop-os/cosmic-panel/compare/af73ac840b72cfe760c4b6e3747fc19d611367e1...efbc15c4c3c189d051c57f273162a5fc88a0e99a

--- pkgs/cosmic-panel/default.nix
+++ pkgs/cosmic-panel/default.nix
@@ -9,17 +9,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-panel";
-  version = "999";
+  version = "epoch-1.0.0-alpha.7-unstable-2025-08-27";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-panel";
-    rev = "af73ac840b72cfe760c4b6e3747fc19d611367e1";
-    hash = "sha256-QcrkfU6HNZ2tWfKsMdcv58HC/PE7b4T14AIep85TWOY=";
+    rev = "efbc15c4c3c189d051c57f273162a5fc88a0e99a";
+    hash = "sha256-TF1fUnN2pDmf7+GA4GLqncHODznnoubja/3stZbdBH4=";
   };
 
-  cargoHash = "sha256-qufOJeWPRjj4GgWNJmQfYaGKeYOQbkTeFzrUSi9QNnQ=";
+  cargoHash = "sha256-VlEbbQTAX05zJYURZym4bBhCtbQ85ujvqLMQNHSz23o=";
 
   nativeBuildInputs = [
     just
