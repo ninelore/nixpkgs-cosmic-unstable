c793c1abc0c5a2b5642a7e251c5de516a29f5a35
Ingo Reitz <9l@9lo.re>
2025-08-28 21:05:33 +0200

    cosmic-settings-daemon: 999 -> epoch-1.0.0-alpha.7-unstable-2025-08-22
    Diff: https://github.com/pop-os/cosmic-settings-daemon/compare/bb9eb904e800d09965e33883e4e15dfd011b1d19...3f72461e99ed9acaccd3199d8888cf619dc5f511

--- pkgs/cosmic-settings-daemon/default.nix
+++ pkgs/cosmic-settings-daemon/default.nix
@@ -14,14 +14,14 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-settings-daemon";
-  version = "999";
+  version = "epoch-1.0.0-alpha.7-unstable-2025-08-22";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-settings-daemon";
-    rev = "bb9eb904e800d09965e33883e4e15dfd011b1d19";
-    hash = "sha256-vdhkE5CmgiGYg5TXxN7lLqxjv7apKEKvIscXFIzZfRc=";
+    rev = "3f72461e99ed9acaccd3199d8888cf619dc5f511";
+    hash = "sha256-Y9JtCXTjtIbUJs4UEedeyH7uWOcbHpWJqubkMND1RiU=";
   };
 
   postPatch = ''
@@ -31,7 +31,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
       --replace-fail '/usr/share/themes/adw-gtk3' '${adw-gtk3}/share/themes/adw-gtk3'
   '';
 
-  cargoHash = "sha256-Dzv1SDeZFIa+LFQQ91lO7RBHldsjDnGf+R12Ln2WZwU=";
+  cargoHash = "sha256-9BeC0Y29NOMoEJHKLV3aRHZQbglbLnnTH4uS3h129iw=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [
