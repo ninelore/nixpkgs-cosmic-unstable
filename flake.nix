a2ae73fc48baf07a90ad42c4a885a3aca385ca6d
Ingo Reitz <9l@9lo.re>
2025-09-27 06:10:52 +0200

    modules: add substituter-only module

--- flake.nix
+++ flake.nix
@@ -82,23 +82,24 @@
 
       overlays.default = (import ./pkgs/default.nix inputs);
 
-      nixosModules.default = {
-        nixpkgs.overlays = [
-          # (
-          #   final: prev:
-          #   import ./pkgs {
-          #     inherit inputs;
-          #     system = prev.system;
-          #   }
-          # )
-          self.overlays.default
-        ];
-        nix = {
-          settings = {
-            extra-substituters = [ "https://9lore.cachix.org" ];
-            extra-trusted-public-keys = [ "9lore.cachix.org-1:H2/a1Wlm7VJRfJNNvFbxtLQPYswP3KzXwSI5ROgzGII=" ];
+      nixosModules =
+        let
+          substituter = {
+            nix = {
+              settings = {
+                extra-substituters = [ "https://9lore.cachix.org" ];
+                extra-trusted-public-keys = [ "9lore.cachix.org-1:H2/a1Wlm7VJRfJNNvFbxtLQPYswP3KzXwSI5ROgzGII=" ];
+              };
+            };
+          };
+        in
+        {
+          inherit substituter;
+          default = substituter // {
+            nixpkgs.overlays = [
+              self.overlays.default
+            ];
           };
         };
-      };
     };
 }
